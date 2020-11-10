967dbdd327d5d477eb19093f46d639f96d5b01d5
tymoxx <7777kara@gmail.com>
2020-11-10 23:12:42 +0100

    adds praises massage

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-require('dotenv').config()
 const TelegramBot = require('node-telegram-bot-api');
+require('dotenv').config()
 
 const TOKEN = process.env.TOKEN;
 
@@ -7,8 +7,22 @@ const bot = new TelegramBot(TOKEN, {
     polling: true,
 });
 
+function isNumeric(str) {
+    if (typeof str != "string") return false // we only process strings!
+    return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+        !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
+}
+
+function getRandomPraise() {
+    const praises = ['молоток', 'мужик', 'так тримати'];
+    return praises[Math.floor(Math.random() * praises.length)];
+}
+
 bot.on('message', (msg) => {
+    // console.log(JSON.stringify(msg, null, 2));
     const {id} = msg.chat;
 
-    bot.sendMessage(id, 'привет, ' + msg.chat.first_name);
+    if (isNumeric(msg.text)) {
+        bot.sendMessage(id, getRandomPraise() + ' ' + msg.chat.first_name + ' 💪');
+    }
 });

1af3a2f8bae52114d16f4652ecf547465dd46d54
tymoxx <7777kara@gmail.com>
2020-11-13 18:19:11 +0100

    adds fever check

--- index.js
+++ index.js
@@ -12,13 +12,15 @@ const serverURL = process.env.URL;
 bot.setWebHook(`${serverURL}/bot${TOKEN}`);
 
 
-function isNumeric(str) {
+const isNumeric = (str) => {
     if (typeof str != "string") return false // we only process strings!
     return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
         !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
 }
 
-function getRandomPraise() {
+const isWhole = (num) => num % 1 === 0;
+
+const getRandomPraise = () =>{
     const praises = ['молоток', 'мужик', 'так тримати', 'Титан', '✅ засчитано', '👍 красавчик'];
     return praises[Math.floor(Math.random() * praises.length)];
 }
@@ -28,17 +30,40 @@ function getRandomEmoji() {
     return praises[Math.floor(Math.random() * praises.length)];
 }
 
+const checkIfFever = (text) => {
+    return false;
+}
+
 bot.on('message', (msg) => {
     // console.log(JSON.stringify(msg, null, 2));
-    const {id} = msg.chat;
-
     const createMessage = () => getRandomPraise() + ', ' + (msg.chat.first_name || msg.from.first_name) + ' ' + getRandomEmoji();
+    const createFeverMessage = () => {
+        const messages = ['🖕', 'дулі!', '💩', '🤒'];
+        return messages[Math.floor(Math.random() * messages.length)];
+    };
 
-    if (Number.isInteger(isNumeric(msg.text)) && (parseInt(msg.text) > 4 && parseInt(msg.text) <= 70)) {
+    const isInPushupRange = num => num > 4 && num <= 70;
+
+    const isInFeverRange = num => num > 36.6 && num <= 38.5;
+
+    const {id} = msg.chat;
+    let number = null;
+
+    if (isNumeric(msg.text)) {
+        number = parseFloat(msg.text);
+    } else return null;
+
+    if (Number.isInteger(number) && isInPushupRange(number)) {
         setTimeout(() => {
             bot.sendMessage(id, createMessage(), {
                 disable_notification: true,
             });
-        }, 1000)
+        }, 500)
+    } else if (!Number.isInteger(number) && isInFeverRange(number)) {
+        setTimeout(() => {
+            bot.sendMessage(id, createFeverMessage(), {
+                disable_notification: true,
+            });
+        }, 500)
     }
 });
